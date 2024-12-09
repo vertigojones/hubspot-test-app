436e06481495b74165dbf6a3d11cf04241df7949
Owen Liversidge <owenliversidge@Owens-MacBook-Pro.local>
2024-12-09 13:37:36 +0000

    initial commit

---
+++ src/pages/App.tsx
@@ -0,0 +1,14 @@
+import styled from "styled-components"
+import { FC } from "react"
+
+type ApiPageProps = {}
+
+const ApiPageContainer = styled.div`
+  /* Add styles here if needed */
+`
+
+const ApiPage: FC<ApiPageProps> = () => {
+  return <ApiPageContainer>Hello World</ApiPageContainer>
+}
+
+export default ApiPage
